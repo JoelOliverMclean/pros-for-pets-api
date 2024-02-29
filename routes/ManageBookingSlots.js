3047739139e26d36a93fcc580db2e007e371fac8
Joel Oliver Mclean <joelolivermclean@gmail.com>
2024-02-29 12:12:34 +0000

    Edit Booking Slots

--- routes/ManageBookingSlots.js
+++ routes/ManageBookingSlots.js
@@ -31,18 +31,39 @@ router.post("/", async (req, res) => {
   });
   let start_time = moment(`${startdate} ${starttime}`);
   let end_time = moment(start_time).add(hours, "h").add(minutes, "m");
-  let bookingSlot = await BookingSlot.create({
-    business,
-    start_time,
-    end_time,
-    cost,
-    slots,
-    spaces_left: slots,
-    location,
-    locationLink,
-    info,
-    service,
-  });
+  let bookingSlot = null;
+  if (id) {
+    bookingSlot = await BookingSlot.findById(id).exec();
+    (bookingSlot.start_time = start_time),
+      (bookingSlot.end_time = end_time),
+      (bookingSlot.cost = cost);
+    let bookingCount = bookingSlot.slots - bookingSlot.spaces_left;
+    if (bookingCount > slots) {
+      return res
+        .status(401)
+        .json({ error: "Too many bookings to reduce slots by that amount" });
+    }
+    bookingSlot.slots = slots;
+    bookingSlot.spaces_left = slots - bookingCount;
+    bookingSlot.location = location;
+    bookingSlot.locationLink = locationLink;
+    bookingSlot.info = info;
+    bookingSlot.service = service;
+    await bookingSlot.save();
+  } else {
+    bookingSlot = await BookingSlot.create({
+      business,
+      start_time,
+      end_time,
+      cost,
+      slots,
+      spaces_left: slots,
+      location,
+      locationLink,
+      info,
+      service,
+    });
+  }
   return res.status(200).json(bookingSlot);
 });
 
@@ -61,6 +82,12 @@ router.get("/", async (req, res) => {
   return res
     .status(200)
     .json({ bookingSlots, pages, results, services: business.services });
+});
+
+router.get("/:id", async (req, res) => {
+  let { id } = req.params;
+  let bookingSlot = await BookingSlot.findById(id).populate("service").exec();
+  return res.status(200).json({ bookingSlot });
 });
 
 router.post("/review-request", async (req, res) => {
