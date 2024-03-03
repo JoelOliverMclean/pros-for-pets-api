83d247b4c43cf005906485a2aef32a36f5965b60
Joel Oliver Mclean <joelolivermclean@gmail.com>
2024-03-03 09:52:09 +0000

    Fix db seed function

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,40 +1,42 @@
-const models = require("./models")
-const bcrypt = require("bcryptjs")
+const models = require("./models");
+const bcrypt = require("bcryptjs");
 
 const seed = async () => {
-    let superpermission = await models.Permission.findOne({
-        name: "super"
-    }).exec()
-    if (!superpermission) {
-        superpermission = await models.Permission.create({
-            name: "super",
-            description: "Permission to do anything"
-        })
-    }
+  let superpermission = await models.Permission.findOne({
+    name: "super",
+  }).exec();
+  if (!superpermission) {
+    superpermission = await models.Permission.create({
+      name: "super",
+      description: "Permission to do anything",
+    });
+  }
 
-    let superuser = await models.User.findOne({
-        username: "admin"
-    }).populate("permissions").exec()
-    if (!superuser) {
-        let hashedPassword = await bcrypt.hash(process.env.SUPERUSERPASSWORD, 10)
-        await models.User.create({
-            username: "admin",
-            password: hashedPassword,
-            firstname: "super",
-            lastname: "user"
-        })
-    }
+  let superuser = await models.User.findOne({
+    username: "admin",
+  })
+    .populate("permissions")
+    .exec();
+  if (!superuser) {
+    let hashedPassword = await bcrypt.hash(process.env.SUPERUSERPASSWORD, 10);
+    superuser = await models.User.create({
+      username: "admin",
+      password: hashedPassword,
+      firstname: "super",
+      lastname: "user",
+    });
+  }
 
-    let superuserpermission = await models.UserPermission.findOne({
-        userId: superuser._id,
-        permissionId: superpermission._id
-    }).exec()
-    if (!superuserpermission) {
-        await models.UserPermission.create({
-            userId: superuser._id,
-            permissionId: superpermission._id
-        })
-    }
-}
+  let superuserpermission = await models.UserPermission.findOne({
+    userId: superuser._id,
+    permissionId: superpermission._id,
+  }).exec();
+  if (!superuserpermission) {
+    superuserpermission = await models.UserPermission.create({
+      userId: superuser._id,
+      permissionId: superpermission._id,
+    });
+  }
+};
 
-module.exports = { seed }
+module.exports = { seed };
